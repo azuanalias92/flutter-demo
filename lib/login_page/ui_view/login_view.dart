58ec2a666e5d4f0ccba3c7f2ed5c3932fe9eb93a
Azuan Alias <azuanalias92@gmail.com>
2023-08-23 18:31:04 +0800

    launch code with arguments

--- lib/login_page/ui_view/login_view.dart
+++ lib/login_page/ui_view/login_view.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:developer' as developer;
+//import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
